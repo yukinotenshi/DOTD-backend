d1208727b6f4580a9e814df86e6b3a225e3ec7d4
yukinotenshi <gabrielbentararaphael@gmail.com>
2019-04-24 14:51:40 +0700

    change room id to uppercase

--- misc.py
+++ misc.py
@@ -1,10 +1,10 @@
-from string import ascii_letters
+from string import ascii_uppercase
 from random import choice
 
 
 def generate_random_string(length=30):
     result = ""
     for _ in range(length):
-        result += choice(ascii_letters)
+        result += choice(ascii_uppercase)
 
     return result
